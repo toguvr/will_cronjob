d2e3c03c228d4a3715d878edef5dcc5c49ba22f9
toguvr <augustotf93@gmail.com>
2024-09-06 00:50:24 -0300

    mobile analitycs

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -13,124 +13,125 @@ export type IPushMessage = {
   data: any;
 };
 
-export default class ExpoPushNotificationProvider {
-  private expo: Expo;
+export async function GET() {
+  class ExpoPushNotificationProvider {
+    private expo: Expo;
 
-  constructor() {
-    this.expo = new Expo({ accessToken: process.env.API_ACCESS_TOKEN });
-  }
-
-  async sendNotification(allMessages: IPushMessage[]): Promise<void> {
-    const messages = [] as IPushMessage[];
-    for (const message of allMessages) {
-      // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
-
-      // Check that all your push tokens appear to be valid Expo push tokens
-      if (!Expo.isExpoPushToken(message.to)) {
-        console.error(
-          `Push token ${message.to} is not a valid Expo push token`
-        );
-        continue;
-      }
-
-      // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
-      messages.push({
-        to: message.to,
-        sound: 'default',
-        body: message.body,
-        title: message.title,
-        data: message.data,
-      });
+    constructor() {
+      this.expo = new Expo({ accessToken: process.env.API_ACCESS_TOKEN });
     }
 
-    const chunks = this.expo.chunkPushNotifications(messages);
+    async sendNotification(allMessages: IPushMessage[]): Promise<void> {
+      const messages = [] as IPushMessage[];
+      for (const message of allMessages) {
+        // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
 
-    const tickets: ExpoPushTicket[] = [];
-    (async () => {
-      // Send the chunks to the Expo push notification service. There are
-      // different strategies you could use. A simple one is to send one chunk at a
-      // time, which nicely spreads the load out over time:
-      for (const chunk of chunks) {
-        try {
-          const ticketChunk = await this.expo.sendPushNotificationsAsync(chunk);
-          console.log(ticketChunk);
-          tickets.push(...ticketChunk);
-          // NOTE: If a ticket contains an error code in ticket.details.error, you
-          // must handle it appropriately. The error codes are listed in the Expo
-          // documentation:
-          // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-        } catch (error) {
-          console.error(error);
+        // Check that all your push tokens appear to be valid Expo push tokens
+        if (!Expo.isExpoPushToken(message.to)) {
+          console.error(
+            `Push token ${message.to} is not a valid Expo push token`
+          );
+          continue;
+        }
+
+        // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+        messages.push({
+          to: message.to,
+          sound: 'default',
+          body: message.body,
+          title: message.title,
+          data: message.data,
+        });
+      }
+
+      const chunks = this.expo.chunkPushNotifications(messages);
+
+      const tickets: ExpoPushTicket[] = [];
+      (async () => {
+        // Send the chunks to the Expo push notification service. There are
+        // different strategies you could use. A simple one is to send one chunk at a
+        // time, which nicely spreads the load out over time:
+        for (const chunk of chunks) {
+          try {
+            const ticketChunk = await this.expo.sendPushNotificationsAsync(
+              chunk
+            );
+            console.log(ticketChunk);
+            tickets.push(...ticketChunk);
+            // NOTE: If a ticket contains an error code in ticket.details.error, you
+            // must handle it appropriately. The error codes are listed in the Expo
+            // documentation:
+            // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      })();
+
+      const receiptIds = [];
+      for (const ticket of tickets) {
+        // NOTE: Not all tickets have IDs; for example, tickets for notifications
+        // that could not be enqueued will have error information and no receipt ID.
+        if (ticket?.id) {
+          receiptIds.push(ticket?.id);
         }
       }
-    })();
 
-    const receiptIds = [];
-    for (const ticket of tickets) {
-      // NOTE: Not all tickets have IDs; for example, tickets for notifications
-      // that could not be enqueued will have error information and no receipt ID.
-      if (ticket.id) {
-        receiptIds.push(ticket?.id);
-      }
-    }
+      const receiptIdChunks =
+        this.expo.chunkPushNotificationReceiptIds(receiptIds);
+      (async () => {
+        // Like sending notifications, there are different strategies you could use
+        // to retrieve batches of receipts from the Expo service.
+        for (const chunk of receiptIdChunks) {
+          try {
+            const receipts = await this.expo.getPushNotificationReceiptsAsync(
+              chunk
+            );
+            console.log(receipts);
 
-    const receiptIdChunks =
-      this.expo.chunkPushNotificationReceiptIds(receiptIds);
-    (async () => {
-      // Like sending notifications, there are different strategies you could use
-      // to retrieve batches of receipts from the Expo service.
-      for (const chunk of receiptIdChunks) {
-        try {
-          const receipts = await this.expo.getPushNotificationReceiptsAsync(
-            chunk
-          );
-          console.log(receipts);
-
-          // The receipts specify whether Apple or Google successfully received the
-          // notification and information about an error, if one occurred.
-          for (const receiptId in receipts) {
-            const { status, message, details } = receipts[receiptId];
-            if (status === 'ok') {
-              continue;
-            } else if (status === 'error') {
-              console.error(
-                `There was an error sending a notification: ${message}`
-              );
-              if (details && details.error) {
-                // The error codes are listed in the Expo documentation:
-                // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-                // You must handle the errors appropriately.
-                console.error(`The error code is ${details.error}`);
+            // The receipts specify whether Apple or Google successfully received the
+            // notification and information about an error, if one occurred.
+            for (const receiptId in receipts) {
+              const { status, message, details } = receipts[receiptId];
+              if (status === 'ok') {
+                continue;
+              } else if (status === 'error') {
+                console.error(
+                  `There was an error sending a notification: ${message}`
+                );
+                if (details && details.error) {
+                  // The error codes are listed in the Expo documentation:
+                  // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+                  // You must handle the errors appropriately.
+                  console.error(`The error code is ${details.error}`);
+                }
               }
             }
+          } catch (error) {
+            console.error(error);
           }
-        } catch (error) {
-          console.error(error);
         }
-      }
-    })();
+      })();
+    }
   }
-}
-const supabaseUrl = process.env.API_SUPABASE_URL;
-const supabaseAnonKey = process.env.API_SUPABASE_ANON_KEY;
-console.log(supabaseUrl);
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: false,
-  },
-});
-export const formatDateUtz = (date: Date) => {
-  const offsetDate = new Date(
-    date.getTime() - date.getTimezoneOffset() * 60000
-  );
+  const supabaseUrl = process.env.API_SUPABASE_URL;
+  const supabaseAnonKey = process.env.API_SUPABASE_ANON_KEY;
+  console.log(supabaseUrl);
+  const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+      persistSession: false,
+    },
+  });
+  const formatDateUtz = (date: Date) => {
+    const offsetDate = new Date(
+      date.getTime() - date.getTimezoneOffset() * 60000
+    );
 
-  const formattedDate = offsetDate.toISOString().split('T')[0];
-  return formattedDate;
-};
-
-export async function GET(request) {
-  const today = formatDateUtz(new Date());
-  const { data, error } = await supabase
+    const formattedDate = offsetDate.toISOString().split('T')[0];
+    return formattedDate;
+  };
+  // const today = formatDateUtz(new Date());
+  const { data } = await supabase
     .from('users')
     .select('id, expo_token')
     .not('expo_token', 'is', null); // Apenas usuários com expo_token preenchido
